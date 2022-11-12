8b3926d8b4e908f97877359aae9e9ad08f8a30a5
Ram Chandra Devkota <r.devkota@ostfalia.de>
2022-11-12 11:05:56 +0100

    new series

--- s5/KW42.c
+++ s5/KW42.c
@@ -1,80 +1,143 @@
-#include <stdio.h>  //fuer printf
+ #include<stdio.h> // fuer printf
 
-//Compilerschalter: -fsanitize=address -Wall
+    // Compilerschalter: -fsanitize=address -Wall
 
-//1) Gegeben seien folgende 'Definitionen':
-struct {
+    // 1) Gegeben seien folgende 'Definitionen':
+    struct
+{
     int x;
     int y;
     int z;
 };
-struct xyz2 {
+struct xyz2
+{
     int x;
     int y;
     int z;
 };
-struct {
+struct
+{
     int x;
-    int y; 
+    int y;
     int z;
 } xyz3;
-struct xyz4 {
+struct xyz4
+{
     int x;
     int y;
     int z;
 } xyz4;
-	
+
 #if 0
 a) Legen sie von jedem Datentyp eine neue Variable an, sofern möglich.
-b) Was sagen die einzelnen Definitionen aus? 
-   struct {};           -->
-   struct xyz2 {};      -->
-   struct {} xyz3;      -->
-   struct xyz4 {} xyz4; -->
-c) Wie sieht die Zuweisung zweier Variablen des identischen Datentyps aus?
-d) Wie sieht der Vergleich zweier Variablen des identischen Datentyps aus?
-e) Wie sieht die Zuweisung zweier Variablen unterschiedlichen Datentyps aus (Inhalt der Strukturen ist identisch)? 
-f) Wie sieht der Vergleich zweier Variablen unterschiedlichen Datentyps aus (Inhalt der Strukturen ist identisch)?
-g) Wie sieht eine Initialisierung/Zuweisung von neuen Variablen des Datentyps 'struct xyz4' in folgenden Fällen aus:
-g1) Initialisierung einer Variablen bei der Definition der Variablen mit Werten in der Reihenfolge der Strukturelemente
-g2) Initialisierung einer Variablen bei der Definition der Variablen mit Werte in einer anderen Reihenfolgen, als die Struktur vorgibt (z.B. zuerst z, dann x und dann y)
-g3) Zuweisung einer bereits angelegten Variablen mit einer Konstanten (Stichwort Compound Literal) 
-#endif	
+// kann kein instanz angelegt werden
+struct xyz2 newVariable = {2,3,4};
+//xyz3 kann kein instanz anlegen
+struct xyz4 newVarible ={4,5,6};
 
-//	2) Gegeben seien folgende 'Datentypen'
-struct sub1struct {
-    int  integer;
-    char str[40];
+b) Was sagen die einzelnen Definitionen aus?
+   struct {};           -->  weder zugreifen or instanz anlegen
+   struct xyz2 {};      -->  zugreifen
+   struct {} xyz3;      -->   nur einmal zugegrifen werden
+   struct xyz4 {} xyz4; -->    kann zugreifen und ein instanz würde schon angelegt
+c) Wie sieht die Zuweisung zweier Variablen des identischen Datentyps aus?
+struct xyz2 var1 = {1,2,3};
+struct xyz2 var2 = var1;
+d) Wie sieht der Vergleich zweier Variablen des identischen Datentyps aus?
+// mit memcomp funktion kann mann die zwei variable vergleichen zB:
+// memcomp(var1, var2,seizeof(Var1));
+
+e) Wie sieht die Zuweisung zweier Variablen unterschiedlichen Datentyps aus (Inhalt der Strukturen ist identisch)?
+struct xyz2 var1 = {1,2,3};
+struct xyz2 var2 = {4,5,6};
+
+va1.x = var2.x;
+va1.y = var2.y;
+va1.z = var2.z;
+
+f) Wie sieht der Vergleich zweier Variablen unterschiedlichen Datentyps aus (Inhalt der Strukturen ist identisch)?
+struct xyz2 var1 = {1,2,3};
+struct xyz2 var2 = {4,5,6};
+if(var1.x==var2.x && va1.y==var2.y&&var1.z==var2.z){
+    //return true
+}else{
+    //return false
+}
+
+g) Wie sieht eine Initialisierung/Zuweisung von neuen Variablen des Datentyps 'struct xyz4' in folgenden Fällen aus:
+
+g1) Initialisierung einer Variablen bei der Definition der Variablen mit Werten in der Reihenfolge der Strukturelemente
+struct xyz2 var1= {3,4,5};
+g2) Initialisierung einer Variablen bei der Definition der Variablen mit Werte in einer anderen Reihenfolgen, als die Struktur vorgibt (z.B. zuerst z, dann x und dann y)
+struct xyz2 var1={.x=3,.y=4,.z=5};
+g3) Zuweisung einer bereits angelegten Variablen mit einer Konstanten (Stichwort Compound Literal)
+xyz4 = (struct xyz4){3,4,5};
+#endif
+
+//    2) Gegeben seien folgende 'Datentypen'
+struct sub1struct
+{
+    int integer;  // offset =0
+    char str[40]; // offset =4 bytes
 };
-struct mainstruct {
-    int a;
-    int b[10];
-    struct sub1struct c[3];
-    struct sub2struct {
-        int d;
-        int e;
-        struct sub1struct f;
+struct mainstruct
+{
+    int a;                  // offset = 0
+    int b[10];              // offset =4 bytes
+    struct sub1struct c[3]; // offset = 44bytes
+    struct sub2struct
+    {
+        int d;               // offset =176 bytes (berecnent mit sizeof(substruct))
+        int e;               // offset = 180 bytes
+        struct sub1struct f; // offset = 184;
     } g;
-}h,i[2];
-	
+} h, i[2];
+
 #if 0
 a) Schreiben sie den Debug-Funktion, welche sämtliche Strukturelemente (incl. Substrukturelemente) der Variablen h formatiert ausgibt
 void debug_mainstruct(struct mainstruct var) {
+printf("%i ",var.a);
+for(int i= 0;i<10;i++){
+    printf("%i ",var.b[i]);
+}
+for(int i =0;i<3;i++){
+    printf("%i %s ",var.c[i].integer, var.c[i].str);
+}
+
+for(int i =0;i<3;i++){
+    printf("%i %i %i %s",var.g.d, var.g.e, var.g.f.integer,var.g.f.str);
+}
 
 }
-b) Legen sie von 'mainstruct' eine neue Variable k an und initialisieren sie alle Elemente (incl. Substrukturelemente) bei der Definition dieser Variablen mit einen beliebigen Wert (Zur Selbstkontrolle am besten obige Debug-Funtkion nutzen!)
+b) Legen sie von 'mainstruct' eine neue Variable k an und initialisieren sie alle Elemente
+(incl. Substrukturelemente) bei der Definition dieser Variablen mit einen
+beliebigen Wert (Zur Selbstkontrolle am besten obige Debug-Funtkion nutzen!)
+
+struct mainstruct k = {1,{1,2,3,4,5,6,7,8,9,0},{{1,"einstring"},{2,"einNeueString"},{3,"nochmalEinstring"}},{2,3,{1,"some random string"}}};
+
 Hilfe: Arrays werden in C wie folgt Initialisiert:   int arr[3]={1,2,3};
 c) Überlegen sie sich den benötigten Speicherbedarf, den eine Variable des Datentyps struct mainstruct belegt?
+// die benötigten speicerbedarf für mainstruct 228 bytes
 d) Alle Strukturelemente werden über einen Offset bezogen auf das erste Strukturelement angesprochen. Welchen Offset haben die einzelnen Strukturelemente?
    (Gerne als Kommentar direkt oben bei der Struktdefinition)
+
 #endif
 
 #if 0
 3) Eine Struktur entspricht einer Klasse bei objektorientierten Sprachen. Beschreiben sie mehrere Unterschiede/Gemeinsamkeiten zwischen der Struktur in C und der Klasse in JAVA.
+//Unterschiede
+//struktur kann nicht beide methoden und Variables beinhalten anders als java klassen
+//in java klassen kann mann Private/public deklarieren (encapsulation ist nicht möglish in strukrtur)
+// Polymorphism ist nicht möglish in struktur in C
+
+//gemeinsamkeiten
+//  beide speichern verschiedene variablen
+//  beide können instanziiert werden
+
 #endif
 
 int main(int argc, char *argv[])
 {
-    
+
     return 0;
 }
--- s6/KW43.c
+++ s6/KW43.c
@@ -1,67 +1,158 @@
 #include <stdio.h>
 
-//CompilerSchalter: -fsanitize=address -Wall -Werror
+// CompilerSchalter: -fsanitize=address -Wall -Werror
 
-//1) Realisieren sie für jedes dieser Datentypbeschreibungen einen C-Datentyp
-struct student {   
+// 1) Realisieren sie für jedes dieser Datentypbeschreibungen einen C-Datentyp
+
+enum Nationalität
+{
+        DEUTSCH,
+        ENGLAND,
+        SPANIEN,
+        SONSTIGES,
 };
-struct dozent  {   
+enum Typen
+{
+        STUDENT,
+        DOZENT,
+        HOCHSCHULE,
 };
-struct hochschule {   
+struct Fächer
+{
+        char name[11];
+        float note;
+};
+struct Student
+{
+        char vorname[11];
+        char nachname[11];
+        short nationalität;
+        short semester;
+        struct Fächer f[2];
 };
 
-//2) Realisieren sie einen weiteren Datentypen, welcher alle diese 3 Datentypen 
-//   aufnehmen kann, wobei nur einer von diesen Aktiv sein kann (Stichwort Union). 
-struct element {
-        //enum zu Identifikation des gültigen Unions
-        //Union zur Aufnahme der 3 zuvor definierten Datentypen
+struct Dozent
+{
+        char title[11];
+        char name[11];
+        short nationalität;
+        short ejahr;
+        struct Fächer f[2];
 };
-            
-//3) Mit dem Datentyp Specifier können einzelnen Strukturelementen zusätzliche 
-//   Eigenschaften/Einschränkungen gegeben werden. Überlegen sie sich für jeden 
-//   Specifier mindestens einen sinnvolles Strukturelement (sofern überhaupt möglich) 
-//   und Begründen sie dessen Verwendung. Wenn es keine Verwendung für einen Specifier 
-//   gibt, so bitte auch dieses Begründen
+
+struct Hochschule
+{
+        char name[11];
+        char ort[11];
+        int plz;
+        char strasse[11];
+        long stunden;
+};
+
+union Data
+{
+        struct Student s;
+        struct Dozent d;
+        struct Hochschule h;
+};
+// 2) Realisieren sie einen weiteren Datentypen, welcher alle diese 3 Datentypen
+//    aufnehmen kann, wobei nur einer von diesen Aktiv sein kann (Stichwort Union).
+struct element
+{
+        // enum zu Identifikation des gültigen Unions
+        // Union zur Aufnahme der 3 zuvor definierten Datentypen
+        enum Typen active;
+        union Data d;
+};
+
+// 3) Mit dem Datentyp Specifier können einzelnen Strukturelementen zusätzliche
+//    Eigenschaften/Einschränkungen gegeben werden. Überlegen sie sich für jeden
+//    Specifier mindestens einen sinnvolles Strukturelement (sofern überhaupt möglich)
+//    und Begründen sie dessen Verwendung. Wenn es keine Verwendung für einen Specifier
+//    gibt, so bitte auch dieses Begründen
 #if 0
   - static:  mögliches Strukturelement + Begründung
+  // alle variable könnte mann static machen, da wir hier nur mit ein datai arbeiten haben wir die variable als private deklariert
+
  - extern:   mögliches Strukturelement + Begründung
+ //da wir hier nur ein c datai haben und nicht von anderen datain irgendwas zugreiffen wollen, ist nicht genutzt
+
  - volatile: mögliches Strukturelement + Begründung
+ //weil wir kein multithreading genutzt haben, haben wir kein volatile genutzt
+
  - const:    mögliches Strukturelement + Begründung
+ // die enums könnte mann als const nutzen aber leider "const enum Typen" wirft fehler
 #endif
 
-//4) Legen sie vom Datentyp 'struct element' ein Array mit 9 Elemente an und 
-//   initialisieren sie dieses Array mit Musterdaten (je 3 von jedem Subdatentyp:
-struct element elemente[9]={ 
-/*[0]=*/{    },
-/*[1]=*/{    },
-/*[2]=*/{    },
-/*[3]=*/{    },
-/*[4]=*/{    },
-/*[5]=*/{    },
-/*[6]=*/{    },
-/*[7]=*/{    },
-/*[8]=*/{    },
+// 4) Legen sie vom Datentyp 'struct element' ein Array mit 9 Elemente an und
+//    initialisieren sie dieses Array mit Musterdaten (je 3 von jedem Subdatentyp:
+struct element elemente[9] = {
+    [0] = {STUDENT, {.s = {"Ram", "Devkota", SONSTIGES, 7, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [2] = {STUDENT, {.s = {"Jorg", "Weimar", SONSTIGES, 7, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [1] = {STUDENT, {.s = {"Suzanne", "Bellmar", SONSTIGES, 7, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [3] = {DOZENT, {.d = {"Prof.Dr", "Pero Ahlers", DEUTSCH, 1996, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [4] = {DOZENT, {.d = {"Prof.Dr", "Karandese", SONSTIGES, 7, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [5] = {DOZENT, {.d = {"Prof.Dr", "Justen", SONSTIGES, 7, {{"ftse", 2.0}, {"TheoInf", 2}}}}},
+    [6] = {HOCHSCHULE, {.h = {"Ostfalia", "wof", 38302, "Am Exer", 800}}},
+    [7] = {HOCHSCHULE, {.h = {"UniJena", "Jena", 70468, "Emil", 25000}}},
+    [8] = {HOCHSCHULE, {.h = {"UniBerlin", "Berlin", 1001, "Am Exer", 9000}}},
 };
-    
-//5) Schreiben sie eine Debug-Routine, welche das gesamte Array tabellarisch ausgibt.
-void debug_element(void) {
+
+// 5) Schreiben sie eine Debug-Routine, welche das gesamte Array tabellarisch ausgibt.
+void debug_element(void)
+{
+
         printf(
-"--------+----------+------------+----------+------------+------------+------------+------------\n"
-"Vorname | Nachname | Nationali. | Semester | Fach0-Name | Fach0-Note | Fach1-Name | Fach1-Note\n"
-"Titel   | Name     | Nationali. | EinsJahr | Fach0-Name | Fach0-Note | Fach1-Name | Fach1-Note\n"
-"-       | Ort      | -          | Plz      | Straße     | Studenten  | -          | -\n"
-"--------+----------+------------+----------+------------+------------+------------+------------\n"
-              );
-    //Hinweis:
-    //- Spaltenbreite der Überschrift darf angepasst werden!
-    //- Die Spalten sollen im Ausdruck alle gleich breit sein
+            "-----------+----------+-----------+----------+-----------+-----------+-----------+-----------------\n"
+            "Vorname    | Nachname | Nationali.| Semester | Fach0-Name| Fach0-Note| Fach1-Name| Fach1-Note\n"
+            "Titel      | Name     | Nationali.| EinsJahr | Fach0-Name| Fach0-Note| Fach1-Name| Fach1-Note\n"
+            "-          | Ort      | -         | Plz      | Straße    | Studenten | -         | -\n"
+            "-----------+----------+-----------+----------+-----------+-----------+-----------+-----------------\n");
+        // Hinweis:
+        //- Spaltenbreite der Überschrift darf angepasst werden!
+        //- Die Spalten sollen im Ausdruck alle gleich breit sein
+        for (int i = 0; i < 9; i++)
+        {
+                if (elemente[i].active == STUDENT)
+                {
+                        printf("%11s", elemente[i].d.s.vorname);
+                        printf("%11s", elemente[i].d.s.nachname);
+                        printf("%11i", elemente[i].d.s.nationalität);
+                        printf("%11i", elemente[i].d.s.semester);
+                        printf("%13s", elemente[i].d.s.f[0].name);
+                        printf("%11f", elemente[i].d.s.f[0].note);
+                        printf("%13s", elemente[i].d.s.f[1].name);
+                        printf("%11f", elemente[i].d.s.f[1].note);
+                }
+                else if (elemente[i].active == DOZENT)
+                {
+                        printf("%11s", elemente[i].d.d.title);
+                        printf("%11s", elemente[i].d.d.name);
+                        printf("%11i", elemente[i].d.d.nationalität);
+                        printf("%11i", elemente[i].d.d.ejahr);
+                        printf("%13s", elemente[i].d.d.f[0].name);
+                        printf("%11f", elemente[i].d.d.f[0].note);
+                        printf("%13s", elemente[i].d.d.f[1].name);
+                        printf("%11f", elemente[i].d.d.f[1].note);
+                }
+                else if (elemente[i].active == HOCHSCHULE)
+                {
+                        printf("%11s", "          ");
+                        printf("%11s", elemente[i].d.h.name);
+                        printf("%11s", elemente[i].d.h.ort);
+                        printf("%11i", elemente[i].d.h.plz);
+                        printf("%11s", elemente[i].d.h.strasse);
+                        printf("%11li", elemente[i].d.h.stunden);
+                }
+                printf("\n");
+        }
 }
 
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+        (void)argc;
+        (void)argv;
 
-    debug_element();
-    return 0;
+        debug_element();
+        return 0;
 }
